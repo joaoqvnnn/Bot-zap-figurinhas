cd128864bb06d07bcb5e1aaf0ab9c418c1aca793
joaoqvnnn <empresadefofocas@gmail.com>
2026-09-01 22:04:46 -0300

    Add global utility functions for number handling

---
+++ src/utils/global.js
@@ -0,0 +1,9 @@
+// ==============================================
+// LARI MYSTIC BOT - FUNÇÕES GLOBAIS
+// ==============================================
+
+const { limparNumero, extrairNumero, gerarIdUnico } = require("./helpers");
+
+global.limparNumero = limparNumero;
+global.extrairNumero = extrairNumero;
+global.gerarIdUnico = gerarIdUnico;
